5d8805d170962ef3e415a8a3a4a5b7723169c620
bsirovemil <96075173+bsirovemil@users.noreply.github.com>
2024-01-21 17:57:48 +0400

    Update ViewEquipment.cs

--- GYM_Management_System/ViewEquipment.cs
+++ GYM_Management_System/ViewEquipment.cs
@@ -11,26 +11,43 @@ using System.Windows.Forms;
 
 namespace GYM_Management_System
 {
+    // Declaration of the ViewEquipment class, which is a partial class of Form
     public partial class ViewEquipment : Form
     {
+        // Constructor for the ViewEquipment class
         public ViewEquipment()
         {
+            // Initialize the components of the form
             InitializeComponent();
         }
+
+        // Event handler for the ViewEquipment form's Load event
         private void ViewEquipment_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source = DESKTOP-2FC14BD;Initial Catalog = gym;Integrated Security=true";
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = con;
+            try
+            {
+                // Create a SqlConnection and SqlCommand for database operations
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "Data Source=DESKTOP-2FC14BD;Initial Catalog=gym;Integrated Security=true";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
 
-            cmd.CommandText = "select * from Equipment";
+                // SQL query to select all data from the Equipment table
+                cmd.CommandText = "select * from Equipment";
 
-            SqlDataAdapter DA = new SqlDataAdapter(cmd);
-            DataSet DS = new DataSet();
-            DA.Fill(DS);
+                // Use SqlDataAdapter to execute the command and fill the DataSet
+                SqlDataAdapter DA = new SqlDataAdapter(cmd);
+                DataSet DS = new DataSet();
+                DA.Fill(DS);
 
-            dataGridView1.DataSource = DS.Tables[0];
+                // Display the data in the DataGridView
+                dataGridView1.DataSource = DS.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                // Display an error message if an exception occurs
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
