8b7c447834c25bf5053eb3f13b98a05916ad4853
bsirovemil <96075173+bsirovemil@users.noreply.github.com>
2024-01-21 17:43:10 +0400

    Update Login.cs

--- GYM_Management_System/Login.cs
+++ GYM_Management_System/Login.cs
@@ -8,25 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Namespace declaration for the GYM_Management_System
 namespace GYM_Management_System
 {
+    // Declaration of the Login class, which is a partial class of Form
     public partial class Login : Form
     {
+        // Constructor for the Login class
         public Login()
         {
+            // Initialize the components of the form
             InitializeComponent();
         }
 
+        // Event handler for the login button click
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if(txtBoxUsername.Text == "admin" && txtBoxPassword.Text == "admin")
+            // Check if the entered username and password are both "admin"
+            if (txtBoxUsername.Text == "admin" && txtBoxPassword.Text == "admin")
             {
+                // If credentials are correct, create an instance of Form1 and show it
                 Form1 fm = new Form1();
                 fm.Show();
+                
+                // Hide the current login form
                 this.Hide();
             }
             else
             {
+                // If credentials are incorrect, show an error message
                 MessageBox.Show("Incorrect Username or Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
