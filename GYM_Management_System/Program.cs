b9e50319f9311c7aef48cd8cc1349c420d29c964
bsirovemil <96075173+bsirovemil@users.noreply.github.com>
2024-01-21 17:54:22 +0400

    Update Program.cs

--- GYM_Management_System/Program.cs
+++ GYM_Management_System/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Namespace declaration for the GYM_Management_System
 namespace GYM_Management_System
 {
+    // Internal static class representing the entry point of the program
     internal static class Program
     {
         /// <summary>
@@ -14,8 +16,13 @@ namespace GYM_Management_System
         [STAThread]
         static void Main()
         {
+            // Enable visual styles for the application
             Application.EnableVisualStyles();
+
+            // Set compatible text rendering default for the application
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Run the application, starting with the Login form
             Application.Run(new Login());
         }
     }
